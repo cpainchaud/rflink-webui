522a6c69c2231e359b474b9253925c4d206e3edc
TheStaticTurtle <tuglersamuel@gmail.com>
2021-02-23 19:39:55 +0100

    Template for the the plugins page

--- src/pages/Plugins.vue
+++ src/pages/Plugins.vue
@@ -1,12 +1,37 @@
 <template>
 	<div>
-		Plugins
+		<h1 class="page-title" style="text-align: center;">Plugins</h1>
+		<div class="container">
+			<h2 style="margin: 5px 0;">OTA</h2>
+			<input type="submit" class="btn-green" value="Save">
+			<table>
+				<tr v-for="plugin in plugins" :key="plugin.key">
+					<td>{{ plugin.name }}</td>
+					<th><label class="switch" style="margin: 0 5px">
+						<input v-model="plugin.enabled" type="checkbox">
+						<span class="slider round"></span>
+					</label></th>
+				</tr>
+			</table>
+			<input type="submit" class="btn-green" value="Save">
+		</div>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: "Plugins"
+		name: "Plugins",
+		data() {
+			return {
+				plugins: [
+					{name:"Plugin A", key:"azerty", enabled:true},
+					{name:"Plugin B", key:"qwerty", enabled:false},
+					{name:"Plugin C", key:"abcdef", enabled:true},
+					{name:"Plugin D", key:"aqwzsx", enabled:false},
+					{name:"Plugin F", key:"qsdfgh", enabled:false},
+				]
+			}
+		}
 	}
 </script>
 
