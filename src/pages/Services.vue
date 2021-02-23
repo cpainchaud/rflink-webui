9bb8a888c544c458509604558934bbb7a970b389
TheStaticTurtle <tuglersamuel@gmail.com>
2021-02-23 20:10:45 +0100

    Created the model for the services

--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -7,7 +7,7 @@
 				<tr>
 					<td>Enabled</td>
 					<th><label class="switch" style="margin: 0 5px">
-						<input type="checkbox">
+						<input v-model="model.serial2net.enabled" type="checkbox">
 						<span class="slider round"></span>
 					</label></th>
 				</tr>
@@ -25,44 +25,38 @@
 						<tr>
 							<td>Enabled</td>
 							<th><label class="switch" style="margin: 0 5px">
-								<input type="checkbox">
+								<input v-model="model.mqtt.enabled" type="checkbox">
 								<span class="slider round"></span>
 							</label></th>
 						</tr>
 						<tr>
 							<td>Server</td>
 							<th><label>
-								<input required placeholder="192.168.1.xxx" type="text">
+								<input v-model="model.mqtt.server"  required placeholder="192.168.1.xxx" type="text">
 							</label></th>
 						</tr>
 						<tr>
 							<td>Port</td>
 							<th><label>
-								<input required placeholder="1883" type="number">
+								<input v-model="model.mqtt.port"  required placeholder="1883" type="number">
 							</label></th>
 						</tr>
 						<tr>
 							<td>ID</td>
 							<th><label>
-								<input required placeholder="ESP8266-RFLink_xxx" type="text">
+								<input v-model="model.mqtt.id"  required placeholder="ESP8266-RFLink_xxx" type="text">
 							</label></th>
 						</tr>
 						<tr>
 							<td>User</td>
 							<th><label>
-								<input required placeholder="rflink" type="text">
+								<input v-model="model.mqtt.user"  required placeholder="rflink" type="text">
 							</label></th>
 						</tr>
 						<tr>
 							<td>Password</td>
 							<th><label>
-								<input required placeholder="*************" type="password">
-							</label></th>
-						</tr>
-						<tr>
-							<td>Server</td>
-							<th><label>
-								<input required placeholder="192.168.1.xxx" type="text">
+								<input v-model="model.mqtt.password" required placeholder="*************" type="password">
 							</label></th>
 						</tr>
 
@@ -71,19 +65,19 @@
 						<tr>
 							<td>OUT Topic</td>
 							<th><label>
-								<input required placeholder="/ESP00/msg" type="text">
+								<input v-model="model.mqtt.topics.out" required placeholder="/ESP00/msg" type="text">
 							</label></th>
 						</tr>
 						<tr>
 							<td>IN Topic</td>
 							<th><label>
-								<input required placeholder="/ESP00/cmd" type="text">
+								<input v-model="model.mqtt.topics.in" required placeholder="/ESP00/cmd" type="text">
 							</label></th>
 						</tr>
 						<tr>
 							<td>LWT Topic</td>
 							<th><label>
-								<input required placeholder="/ESP00/lwt" type="text">
+								<input v-model="model.mqtt.topics.lwt" required placeholder="/ESP00/lwt" type="text">
 							</label></th>
 						</tr>
 					</table>
@@ -96,7 +90,29 @@
 
 <script>
 	export default {
-		name: "Services"
+		name: "Services",
+		data() {
+			return {
+				model: {
+					serial2net: {
+						enabled: true
+					},
+					mqtt: {
+						enabled: true,
+						server: "",
+						port: 1883,
+						id: "",
+						user: "",
+						password: "",
+						topics: {
+							out: "",
+							in: "",
+							lwt: "",
+						}
+					}
+				}
+			}
+		}
 	}
 </script>
 
