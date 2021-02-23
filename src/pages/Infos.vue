1f9ea8f33278d6ccaad09b6a94efba1b0fdf5e91
TheStaticTurtle <tuglersamuel@gmail.com>
2021-02-23 19:25:56 +0100

    Added the info/credits page

---
+++ src/pages/Infos.vue
@@ -0,0 +1,19 @@
+<template>
+	<div style="display: flex; height: 100%; justify-content: center; align-items: center; flex-direction: column">
+		<h1>RFLink-ESP</h1>
+		<span>Version firmware: X.X.X</span>
+		<span>Version webui: 0.1.0</span>
+		<span>Firmware: <a href="https://github.com/couin3/RFLink">https://github.com/couin3/RFLink</a> (<a href="https://github.com/couin3/RFLink/graphs/contributors">Contributors</a>)</span>
+		<span>WebUI: <a href="https://github.com/cpainchaud/rflink-webui">https://github.com/cpainchaud/rflink-webui</a> (<a href="https://github.com/cpainchaud/rflink-webui/graphs/contributors">Contributors</a>)</span>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Infos"
+	}
+</script>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ import Signal from './pages/Signal'
 import Plugins from './pages/Plugins'
 import Services from './pages/Services'
 import Firmware from './pages/Firmware'
+import Infos from './pages/Infos'
 
 Vue.use(Router);
 
@@ -21,6 +22,7 @@ export const router = new Router({
     { name:"Plugins",     path: '/plugins',  component: Plugins },
     { name:"Services",    path: '/services', component: Services },
     { name:"Firmware",    path: '/firmware', component: Firmware },
+    { name:"Infos",       path: '/infos', component: Infos },
 
     // otherwise redirect to home
     { path: '*', redirect: '/home' }
