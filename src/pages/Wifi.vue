19fcb82550e8d11fb00bf7e6787154d97894d3d1
TheStaticTurtle <tuglersamuel@gmail.com>
2021-02-25 00:16:26 +0100

    Added configuration for the wifi ap/client

--- src/pages/Wifi.vue
+++ src/pages/Wifi.vue
@@ -3,42 +3,106 @@
 	<div>
 		<h1 class="page-title" style="text-align: center;">Wifi</h1>
 		<div class="container">
+			<h2 style="margin: 5px 0;">Client</h2>
 			<table>
+				<tr>
+					<td>Active</td>
+					<th><label class="switch" style="margin: 0 5px">
+						<input v-model="model.client.active" type="checkbox">
+						<span class="slider round"></span>
+					</label></th>
+				</tr>
 				<tr>
 					<td>DHCP</td>
 					<th><label class="switch" style="margin: 0 5px">
-						<input v-model="model.dhcp" type="checkbox">
+						<input :disabled="!model.client.active" v-model="model.client.dhcp" type="checkbox">
 						<span class="slider round"></span>
 					</label></th>
 				</tr>
 
 				<tr>
+					<td>SSID</td>
+					<th><label>
+						<input :disabled="!model.client.active" v-model="model.client.ssid" required placeholder="MonWifi" type="text">
+					</label></th>
+				</tr>
+				<tr>
+					<td>PSK</td>
+					<th><label>
+						<input :disabled="!model.client.active" v-model="model.client.password" required placeholder="***********" type="password">
+					</label></th>
+				</tr>
+				<tr>
 					<td>IP</td>
 					<th><label>
-						<input :disabled="model.dhcp" v-model="model.ip" required placeholder="192.168.1.xxx" type="text">
+						<input :disabled="model.client.dhcp || !model.client.active" v-model="model.client.ip" required placeholder="192.168.1.xxx" type="text">
 					</label></th>
 				</tr>
 				<tr>
 					<td>DNS</td>
 					<th><label>
-						<input :disabled="model.dhcp" v-model="model.dns" required placeholder="192.168.1.xxx" type="text">
+						<input :disabled="model.client.dhcp || !model.client.active" v-model="model.client.dns" required placeholder="192.168.1.xxx" type="text">
 					</label></th>
 				</tr>
 				<tr>
 					<td>Gateway</td>
 					<th><label>
-						<input :disabled="model.dhcp" v-model="model.gateway" required placeholder="192.168.1.xxx" type="text">
+						<input :disabled="model.client.dhcp || !model.client.active" v-model="model.client.gateway" required placeholder="192.168.1.xxx" type="text">
 					</label></th>
 				</tr>
 				<tr>
 					<td>Subnet</td>
 					<th><label>
-						<input :disabled="model.dhcp" v-model="model.subnet" required placeholder="255.255.255.0" type="text">
+						<input :disabled="model.client.dhcp || !model.client.active" v-model="model.client.subnet" required placeholder="255.255.255.0" type="text">
 					</label></th>
 				</tr>
 			</table>
 			<input type="submit" class="btn-green" value="Save">
 		</div>
+
+		<div class="container">
+		<h2 style="margin: 5px 0;">AP</h2>
+		<table>
+			<tr>
+				<td>Active</td>
+				<th><label class="switch" style="margin: 0 5px">
+					<input v-model="model.ap.active" type="checkbox">
+					<span class="slider round"></span>
+				</label></th>
+			</tr>
+			<tr>
+				<td>SSID</td>
+				<th><label>
+					<input :disabled="!model.ap.active" v-model="model.client.ssid" required placeholder="MonWifi" type="text">
+				</label></th>
+			</tr>
+			<tr>
+				<td>PSK</td>
+				<th><label>
+					<input :disabled="!model.ap.active" v-model="model.client.password" required placeholder="***********" type="password">
+				</label></th>
+			</tr>
+			<tr>
+				<td>IP</td>
+				<th><label>
+					<input :disabled="!model.ap.active" v-model="model.client.ip" required placeholder="192.168.1.xxx" type="text">
+				</label></th>
+			</tr>
+			<tr>
+				<td>Network</td>
+				<th><label>
+					<input :disabled="!model.ap.active" v-model="model.client.gateway" required placeholder="192.168.1.xxx" type="text">
+				</label></th>
+			</tr>
+			<tr>
+				<td>Subnet</td>
+				<th><label>
+					<input :disabled="!model.ap.active" v-model="model.client.subnet" required placeholder="255.255.255.0" type="text">
+				</label></th>
+			</tr>
+		</table>
+		<input type="submit" class="btn-green" value="Save">
+	</div>
 	</div>
 </template>
 
@@ -48,11 +112,24 @@
 		data() {
 			return {
 				model: {
-					dhcp: true,
-					ip: "",
-					dns: "",
-					subnet: "",
-					gateway: "",
+					client: {
+						active: true,
+						ssid: "",
+						password: "",
+						dhcp: true,
+						ip: "",
+						dns: "",
+						subnet: "",
+						gateway: "",
+					},
+					ap: {
+						active: true,
+						ssid: "",
+						password: "",
+						ip: "",
+						subnet: "",
+						gateway: "",
+					}
 				}
 			}
 		}
