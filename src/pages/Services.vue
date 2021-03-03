2c1f6d6a7cec60d17ad11bad5dd1f34f2ce5f522
TheStaticTurtle <tuglersamuel@gmail.com>
2021-03-03 16:09:30 +0100

    Added the mqtt configuration using old model system and moved it to the config one

--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -1,21 +1,21 @@
 <template>
 	<div>
 		<h1 class="page-title" style="text-align: center;">Services</h1>
-		<div class="container">
+		<!-- <div class="container" v-if="config.serial2net !== {}">
 			<h2 style="margin: 5px 0;">Serial2Net</h2>
 			<table>
 				<tr>
 					<td>Enabled</td>
 					<th><label class="switch" style="margin: 0 5px">
-						<input v-model="model.serial2net.enabled" type="checkbox">
+						<input v-model="config.serial2net.enabled" type="checkbox">
 						<span class="slider round"></span>
 					</label></th>
 				</tr>
 			</table>
 			<input type="submit" class="btn-green" @click="save_config" value="Save">
-		</div>
+		</div> -->
 
-		<div class="container ">
+		<div class="container" v-if="config.mqtt !== {}">
 			<h2 style="margin: 5px 0;">MQTT</h2>
 			<div class="grid-container">
 				<div class="grid-item">
@@ -25,38 +25,38 @@
 						<tr>
 							<td>Enabled</td>
 							<th><label class="switch" style="margin: 0 5px">
-								<input v-model="model.mqtt.enabled" type="checkbox">
+								<input v-model="config.mqtt.enabled" type="checkbox">
 								<span class="slider round"></span>
 							</label></th>
 						</tr>
 						<tr>
 							<td>Server</td>
 							<th><label>
-								<input :disabled="!model.mqtt.enabled" v-model="config.mqtt.server"  required placeholder="192.168.1.xxx" type="text">
+								<input :disabled="!config.mqtt.enabled" v-model="config.mqtt.server"  required placeholder="192.168.1.xxx" type="text">
 							</label></th>
 						</tr>
 						<tr>
 							<td>Port</td>
 							<th><label>
-								<input :disabled="!model.mqtt.enabled" v-model.number="config.mqtt.port" required placeholder="1883" type="number">
+								<input :disabled="!config.mqtt.enabled" v-model.number="config.mqtt.port" required placeholder="1883" type="number">
 							</label></th>
 						</tr>
 						<tr>
 							<td>ID</td>
 							<th><label>
-								<input :disabled="!model.mqtt.enabled" v-model="config.mqtt.id"  required placeholder="ESP8266-RFLink_xxx" type="text">
+								<input :disabled="!config.mqtt.enabled" v-model="config.mqtt.id"  required placeholder="ESP8266-RFLink_xxx" type="text">
 							</label></th>
 						</tr>
 						<tr>
 							<td>User</td>
 							<th><label>
-								<input :disabled="!model.mqtt.enabled" v-model="config.mqtt.user"  required placeholder="rflink" type="text">
+								<input :disabled="!config.mqtt.enabled" v-model="config.mqtt.user"  required placeholder="rflink" type="text">
 							</label></th>
 						</tr>
 						<tr>
 							<td>Password</td>
 							<th><label>
-								<input :disabled="!model.mqtt.enabled" v-model="config.mqtt.password"  required placeholder="*************" type="password">
+								<input :disabled="!config.mqtt.enabled" v-model="config.mqtt.password"  required placeholder="*************" type="password">
 							</label></th>
 						</tr>
 
@@ -65,26 +65,26 @@
 						<tr>
 							<td>OUT Topic</td>
 							<th><label>
-								<input :disabled="!model.mqtt.enabled" v-model="config.mqtt.topic_in" required placeholder="/ESP00/msg" type="text">
+								<input :disabled="!config.mqtt.enabled" v-model="config.mqtt.topic_in" required placeholder="/ESP00/msg" type="text">
 							</label></th>
 						</tr>
 						<tr>
 							<td>IN Topic</td>
 							<th><label>
-								<input :disabled="!model.mqtt.enabled" v-model="config.mqtt.topic_out" required placeholder="/ESP00/cmd" type="text">
+								<input :disabled="!config.mqtt.enabled" v-model="config.mqtt.topic_out" required placeholder="/ESP00/cmd" type="text">
 							</label></th>
 						</tr>
 						<tr>
 							<td>LWT Enabled</td>
 							<th><label class="switch" style="margin: 0 5px">
-								<input v-model="model.mqtt.lwt_enabled" :disabled="!model.mqtt.enabled"  type="checkbox">
+								<input v-model="config.mqtt.lwt_enabled" :disabled="!config.mqtt.enabled"  type="checkbox">
 								<span class="slider round"></span>
 							</label></th>
 						</tr>
 						<tr>
 							<td>LWT Topic</td>
 							<th><label>
-								<input :disabled="!model.mqtt.enabled || !model.mqtt.lwt_enabled" v-model="config.mqtt.topic_lwt" required placeholder="/ESP00/lwt" type="text">
+								<input :disabled="!config.mqtt.enabled || !config.mqtt.lwt_enabled" v-model="config.mqtt.topic_lwt" required placeholder="/ESP00/lwt" type="text">
 							</label></th>
 						</tr>
 					</table>
@@ -98,18 +98,6 @@
 <script>
 	export default {
 		name: "Services",
-		data() {
-			return {
-				model: {
-					serial2net: {
-						enabled: false
-					},
-					mqtt: {
-						enabled: false,
-					}
-				}
-			}
-		}
 	}
 </script>
 
