10a346d268cc06500fd96487a37a473746232632
TheStaticTurtle <tuglersamuel@gmail.com>
2021-03-11 22:52:32 +0100

    Added firmware file upload

---
+++ src/components/Progress.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="container">
+		<div class="bar" :style="style">
+
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Progress",
+		props: {
+			percentage: {
+				required: true,
+				type: Number
+			},
+			color: {
+				type: String,
+				default: "red"
+			}
+		},
+		computed: {
+			style() {
+				return {
+					"width": this.percentage+"%",
+					"background-color": this.color,
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.container {
+		height: 15px;
+		background-color: #e8e8e8;
+		width: 100%;
+		padding: 0;
+		margin:0;
+		border: none;
+		border-radius: 5px;
+	}
+	.bar {
+		height: 100%;
+		border-radius: 5px;
+		-webkit-transition: width 0.2s ease-in-out;
+		-moz-transition: width 0.2s ease-in-out;
+		-o-transition: width 0.2s ease-in-out;
+		transition: width 0.2s ease-in-out;
+	}
+</style>
--- src/pages/Firmware.vue
+++ src/pages/Firmware.vue
@@ -20,13 +20,18 @@
 			</table>
 			<input type="submit" class="btn-green" value="Save">
 		</div>
+
 		<div class="container">
-			<h2 style="margin: 5px 0 15px 0;">Upload update</h2>
+			<h2 style="margin: 5px 0 15px 0;">Update via upload</h2>
 
-			<input type="file" id="myfile" name="myfile"><br><br>
+			<input ref="file" style="margin-bottom: 5px; margin-right: 10px; display: inline-block" type="file" id="myfile" v-on:change="handleFileUpload" name="myfile">
+			<span v-if="file !== null" style="padding: 0; margin: 5px 0 10px 0;">{{ file.name }}</span>
+			<p v-if="md5 !== ''" style="padding: 0; margin: 5px 0 10px 0;"><b>MD5:</b> {{ md5 }}</p>
 
-			<input type="submit" class="btn-green" value="Flash">
+			<Progress v-if="uploadPercentage>0" :percentage="this.uploadPercentage"></Progress>
+			<input type="submit" @click="submitFile" class="btn-green" value="Flash">
 		</div>
+
 		<div class="container">
 			<h2 style="margin: 5px 0 15px 0;">Update via HTTP URL</h2>
 				<tr>
@@ -42,11 +47,76 @@
 </template>
 
 <script>
+	import axios from "axios";
 	import {api_mixin} from "../api_mixin";
+	import Swal from "sweetalert2";
+	import Progress from "../components/Progress";
+	import BMF from 'browser-md5-file';
+
+	const bmf = new BMF();
 
 	export default {
 		name: "Firmware",
+		components: {Progress},
 		mixins: [api_mixin],
+		data() {
+			return {
+				uploadPercentage: 0,
+				md5: "",
+				file: null
+			}
+		},
+		methods: {
+			handleFileUpload(){
+				this.file = this.$refs.file.files[0];
+				console.log(this.file)
+				bmf.md5(this.file, (err, md5) => {
+					this.md5 = md5
+				}, progress => {
+					if(progress<1) this.md5 = "Calculating " + Math.round(progress*10000)/100 + "%";
+				});
+			},
+			submitFile(){
+				if(this.file == null) {
+					return Swal.fire({
+						title: 'Error!',
+						html: 'You didn\'t select a file',
+						icon: 'error',
+						confirmButtonText: 'Continue'
+					})
+				}
+
+				let formData = new FormData();
+
+				formData.append('upload', this.file);
+				formData.append('md5', this.md5);
+
+				axios.post( '/api/firmware/update', formData, {
+						headers: { 'Content-Type': 'multipart/form-data' },
+						onUploadProgress: function( progressEvent ) {
+							this.uploadPercentage = progressEvent.loaded / progressEvent.total;
+						}.bind(this)
+					}
+				).then(function(){
+					Swal.fire({
+						title: 'Success!',
+						html: 'Operation is a success, the esp will now reboot',
+						icon: 'success',
+						confirmButtonText: 'Reload'
+					}).then(() => {
+						window.reload()
+					})
+				}).catch(error => {
+					console.error(error)
+					Swal.fire({
+						title: 'Error!',
+						html: 'A network error occured while saving: '+error,
+						icon: 'error',
+						confirmButtonText: 'Continue'
+					})
+				});
+			},
+		}
 	}
 </script>
 
