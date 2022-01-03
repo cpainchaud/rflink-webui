055a92ca47bcb13ed55993ecc2718e05386464d1
Christophe Painchaud <shellescape@gmail.com>
2022-01-03 14:12:26 +0100

    changed firmware URLs in Infos Vue

--- src/pages/Infos.vue
+++ src/pages/Infos.vue
@@ -3,7 +3,7 @@
 		<h1>RFLink-ESP</h1>
 		<span>Version firmware: X.X.X</span>
 		<span>Version webui: 0.1.0</span>
-		<span>Firmware: <a href="https://github.com/couin3/RFLink">https://github.com/couin3/RFLink</a> (<a href="https://github.com/couin3/RFLink/graphs/contributors">Contributors</a>)</span>
+		<span>Firmware: <a href="https://github.com/couin3/RFLink">https://github.com/cpainchaud/RFLink32</a> (<a href="https://github.com/cpainchaud/RFLink32/graphs/contributors">Contributors</a>)</span>
 		<span>WebUI: <a href="https://github.com/cpainchaud/rflink-webui">https://github.com/cpainchaud/rflink-webui</a> (<a href="https://github.com/cpainchaud/rflink-webui/graphs/contributors">Contributors</a>)</span>
 	</div>
 </template>
